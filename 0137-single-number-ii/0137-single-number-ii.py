64c02e7cf8b3eef98142fe253fa9bedfc1a315d6
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-05-04 01:28:08 +0300

    Time: 665 ms (10.9%), Space: 18.6 MB (82.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,13 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        stack1={}
         for i in nums:
-            if i  in stack1:
-                stack1[i]+=1
-            else:
+            if nums.count(i)==1:
+                return i
 
-                stack1[i]=1
-        return min(stack1 , key=stack1.get)
+
+
+        # stack1={}
+        # for i in nums:
+        #     if i  in stack1:
+        #         stack1[i]+=1
+        #     else:
+
+        #         stack1[i]=1
+        # return min(stack1 , key=stack1.get)
            
 
         
