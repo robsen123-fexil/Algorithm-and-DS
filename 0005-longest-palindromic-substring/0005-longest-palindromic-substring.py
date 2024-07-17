3370797befe84fdfe44a97bea3df2ae9bf74a98a
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-18 00:23:40 +0300

    Time: 9129 ms (5.01%), Space: 236.4 MB (5.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -14,6 +14,22 @@ class Solution:
         else:
             return resu[0]
         return resu[0]
+
+        # result=[]
+        # def ispalindrome(res):
+        #     return res==res[::-1]
+        
+        # for i in range(len(s)):
+        #     r=i+1
+        #     strs=''
+        #     while r<len(s)+1:
+        #         res=s[i:r]
+        #         if ispalindrome(res):
+        #             result.append(res)
+        #         r+=1
+        # result=sorted(result , key= lambda x:len(x) , reverse=True)
+        # print(result)
+        # return result[0] if len(result)!=0 else ""
         
 
 
