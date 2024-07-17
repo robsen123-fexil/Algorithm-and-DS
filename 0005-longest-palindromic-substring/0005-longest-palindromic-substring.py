260b482b338500ad7037375d2f6b9f0d25bbb92a
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-18 00:29:32 +0300

    Time: 9987 ms (5.01%), Space: 236.4 MB (5.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,10 +4,13 @@ class Solution:
         def ispalindrom(res):
             return res==res[::-1]
         for i in range(len(s)):
-            for j in range(i+1 , len(s)+1):
+            # for j in range(i+1 , len(s)+1):
+            j=i+1
+            while j<len(s)+1:
                 res=s[i:j]
                 if ispalindrom(res) :
                     result.append(res)
+                j+=1
         resu=sorted(result , key=lambda x:len(x)  , reverse=True)
         if len(resu)==0:
             return ""
