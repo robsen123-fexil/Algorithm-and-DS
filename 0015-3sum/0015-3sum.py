393b26710a5118aa7d3018ffaf10324fc9eddfe2
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-18 23:56:15 +0300

    Time: 643 ms (76.44%), Space: 20.6 MB (78.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,66 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        length=len(nums)
-        res=[] #[-4,-1-1,0,1,2]
+        # length=len(nums)
+        # res=[] 
+        # nums.sort()
+        # for i in range(length-2):
+        #     if i>0 and nums[i]==nums[i-1]:
+        #        continue
+        #     l=i+1
+        #     r=length-1
+        #     while l<r:
+        #         total=nums[i]+nums[l]+nums[r]
+        #         if total<0:
+        #             l=l+1
+        #         elif total>0:
+        #             r=r-1
+        #         else:
+        #             res.append([nums[i], nums[l], nums[r]]) 
+        #             while l<r and nums[l]==nums[l+1]:
+        #                 l=l+1
+        #             while l<r and nums[r]==nums[r-1]:
+        #                 r=r-1   
+
+        #             l=l+1
+        #             r=r-1     
+
+        # return res  
+        # result=[]
+        # nums.sort()
+        # for i , a in enumerate(nums):
+        #     if i>0 and a==nums[i-1]:
+        #         continue
+        #     hsh={}            
+        #     for j  , value in enumerate(nums[i+1:]):
+        #         b=-a-value
+        #         if b in hsh:
+        #             if not [a , value , b] in result:
+        #                 result.append([a , value , b])
+        #         hsh[value]=j
+        # return result
+        result=[]
         nums.sort()
-        for i in range(length-2):
-            if i>0 and nums[i]==nums[i-1]:
-               continue
+        for i , a in enumerate(nums):
+            if i>0 and a==nums[i-1]:
+                continue
             l=i+1
-            r=length-1
+            r=len(nums)-1
             while l<r:
-                total=nums[i]+nums[l]+nums[r]
+                total=a+nums[l]+nums[r]
                 if total<0:
-                    l=l+1
+                    l+=1
                 elif total>0:
-                    r=r-1
+                    r-=1
                 else:
-                    res.append([nums[i], nums[l], nums[r]]) 
-                    while l<r and nums[l]==nums[l+1]:
-                        l=l+1
-                    while l<r and nums[r]==nums[r-1]:
-                        r=r-1   
+                    result.append([a , nums[l] , nums[r] ])
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+                    
+                    r-=1
+                    while nums[r]==nums[r+1] and l<r:
+                        r-=1
 
-                    l=l+1
-                    r=r-1     
+        return result
 
-        return res              
+
