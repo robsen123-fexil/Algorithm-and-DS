3efcc7b1d1a6fd957741b42e77c4f489b8b70268
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-21 00:39:23 +0300

    Time: 32 ms (84.37%), Space: 16.5 MB (48.59%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def fibo(n):
-            if n<=1:
-                return n
-            else:
-                return fibo(n-1)+fibo(n-2)
-        return fibo(n)
+        result=[0 , 1]
+        for i in range(n):
+            result.append(result[-2]+result[-1])
+        return result[-2]
+        
