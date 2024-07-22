ae8fe153afe5ad77af80c556bfe8b925584b3a71
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-22 15:48:03 +0300

    Time: 44 ms (26.4%), Space: 16.5 MB (80.68%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,27 +5,51 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr=[]
+        # arr=[]
+        # temp=head
+        # while temp:
+        #     arr.append(temp.val)
+        #     temp=temp.next
+        # hsh={}
+        # print(arr)
+        # for i in arr:
+        #     if i in hsh:
+        #         hsh[i]+=1
+        #     else:
+        #         hsh[i]=1
+        # result=[]
+        # print(hsh)
+        # for key , value in hsh.items():
+        #     if value==1:
+        #         result.append(key)
+        # print(result)
+        # dummy=ListNode(0)
+        # tail=dummy
+        # for i in result:
+        #     tail.next=ListNode(i)
+        #     tail=tail.next
+        # return dummy.next
+        if head==None:
+            return head
         temp=head
-        while temp:
-            arr.append(temp.val)
-            temp=temp.next
-        hsh={}
-        print(arr)
-        for i in arr:
-            if i in hsh:
-                hsh[i]+=1
-            else:
-                hsh[i]=1
-        result=[]
-        print(hsh)
-        for key , value in hsh.items():
-            if value==1:
-                result.append(key)
-        print(result)
         dummy=ListNode(0)
         tail=dummy
-        for i in result:
-            tail.next=ListNode(i)
+        prev=ListNode(None)
+        # while temp and temp.next:
+        #     if temp.val !=temp.next.val and temp.val!=prev:
+        #         tail.next=ListNode(temp.val)
+        #         tail=tail.next
+        #     temp=temp.next
+        #     prev=temp.val
+        # return dummy.next
+        while temp and temp.next:
+            if prev!=temp.val and temp.val!=temp.next.val:
+                tail.next=ListNode(temp.val)
+                tail=tail.next
+            prev=temp.val
+            temp=temp.next
+        if temp.val!=prev:
+            tail.next=ListNode(temp.val)
             tail=tail.next
+            
         return dummy.next
