6695924b421eb59e860fe4563a76d131fb3f4d5c
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-25 23:31:16 +0300

    Time: 487 ms (5.02%), Space: 20.6 MB (34.35%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,33 +2,22 @@ class MinStack:
 
     def __init__(self):
         self.stack=[]
-        self.minstack=[]
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if self.minstack:
-            x=self.minstack[-1]
-            self.minstack.append(min(val, x))
-        else:
-            self.minstack.append(val)
-        
-        
-
         
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minstack.pop()
         
-
 
     def top(self) -> int:
         return self.stack[-1]
         
 
     def getMin(self) -> int:
-        return self.minstack[-1]
+        return min(self.stack)
         
 
 
