61d4d4b5e172ecd649073b18722b3fdd82e4ad6e
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-07-25 17:06:33 +0300

    Time: 38 ms (39.93%), Space: 16.7 MB (11.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,34 +2,71 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
         s=list(s)
+        # for i in s:
+        #     if i =='(' or i == '{' or i == '[':
+        #         stack.append(i)
+        #     else:
+        #         if i==')':
+        #             if stack and stack[-1]=='(':
+        #                 stack.pop()
+        #             else:
+        #                 return False 
+        #         if i=='}':
+        #             if stack and stack[-1]=='{':
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         if i==']':
+        #             if stack and stack[-1]=='[':
+        #                 stack.pop()
+        #             else:
+        #                 return False 
+        # if not stack:
+        #     return True       
+        # for i in s:
+        #     if stack :
+        #         if stack[-1]=='(' or stack[-1]=='[' or stack[-1]=='}':
+        #             if i==')':
+        #                 stack.append(i)
+        #             else:
+        #                 return False
+        #         if stack[-1]=='{':
+        #             if i=='}':
+        #                 stack.append(i)
+        #             else:
+        #                 return False
+        #         if stack[-1]=='[':
+        #             if i==']':
+        #                 stack.append(i)
+        #             else:
+        #                 return False
+                    
+                
+        #     else:
+        #         stack.append(i)
+        # return True
+        if len(s)==1:
+            return False
         for i in s:
-            if i =='(' or i == '{' or i == '[':
+            if i == '{' or i=='[' or i=='(':
                 stack.append(i)
             else:
-                if i==')':
-                    if stack and stack[-1]=='(':
+                if stack:
+                    if i==')' and stack[-1]=='(':
                         stack.pop()
-                    else:
-                        return False 
-                if i=='}':
-                    if stack and stack[-1]=='{':
+                    
+                    elif  i=='}' and stack[-1]=='{':
+                        stack.pop()
+                    
+                    elif i==']' and stack[-1]=='[':
                         stack.pop()
                     else:
                         return False
-                if i==']':
-                    if stack and stack[-1]=='[':
-                        stack.pop()
-                    else:
-                        return False 
-        if not stack:
-            return True       
- 
-                
-
-                
-
+                else:
+                    stack.append(i)
+        return True if not stack else False
             
 
-        
+                    
 
-
+                
