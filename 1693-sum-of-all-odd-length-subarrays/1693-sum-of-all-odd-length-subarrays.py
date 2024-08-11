7812038924c78d35799a1bd78cbbb69e7db2e125
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-11 18:34:07 +0300

    Time: 95 ms (8.68%), Space: 19.1 MB (5.16%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         def generatesubarray(arr, start, end):
@@ -11,18 +9,12 @@ class Solution:
                 else:
                     result.append(arr[start:end + 1])
                     return appendallsubarrays(arr, start + 1, end, result)
-            
             result = []
             appendallsubarrays(arr, 0, 0, result)
             return result
-        
-        # Generate all subarrays
         all_subarrays = generatesubarray(arr, 0, 0)
-        
-        # Sum the elements of subarrays with odd lengths
         total_sum = 0
         for subarray in all_subarrays:
             if len(subarray) % 2 != 0:
                 total_sum += sum(subarray)
-        
         return total_sum
