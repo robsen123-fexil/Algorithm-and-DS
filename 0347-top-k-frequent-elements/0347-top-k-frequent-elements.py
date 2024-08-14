fbcc5a94f74a40481913b0a13162f2e98381c288
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-15 00:24:00 +0300

    Time: 99 ms (14.78%), Space: 20.8 MB (91.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -12,16 +12,34 @@ class Solution:
         # else:
         #     ans=[key for key, _ in val]
         # return ans
-        hashmap1={}
-        for i in nums:
-            if i in hashmap1:
-                hashmap1[i]+=1
-            else:
-                hashmap1[i]=1
-        val =sorted(hashmap1.items() , key =lambda x:x[1])
-        ans=[]
-        if len(val)>=k:
-            ans=[key for key , _ in val[-k:]]
-        else:
-            ans=[key for key, _ in val]
-        return ans
+        # hashmap1={}
+        # for i in nums:
+        #     if i in hashmap1:
+        #         hashmap1[i]+=1
+        #     else:
+        #         hashmap1[i]=1
+        # val =sorted(hashmap1.items() , key =lambda x:x[1])
+        # ans=[]
+        # if len(val)>=k:
+        #     ans=[key for key , _ in val[-k:]]
+        # else:
+        #     ans=[key for key, _ in val]
+        # return ans
+        count=Counter(nums)
+        count=dict(sorted(count.items() , key =lambda x:x[1] ,  reverse=True) ,)
+        print(count)
+        result=[]
+        l=0
+        sol=[]
+        for key , value in count.items():
+            result.append(key)
+        while k>0:
+            sol.append(result[l])
+            l+=1
+            k-=1
+
+        return sol
+
+
+        result=[]
+        
