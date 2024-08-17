02fa8b885418b087a1b61c9b9db81992bc291063
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-17 22:56:49 +0300

    Time: 41 ms (48.26%), Space: 16.5 MB (88.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,11 +3,34 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # l=r=k=0
+        # for i in range(len(nums2)):
+        #     nums1[m+i]=nums2[i]
+        # print(nums1)
+        # while l<m and r<n:
+        #     if nums1[l]<=nums2[r]:
+        #         nums1[k]=nums1[l]
+        #         l+=1
+        #     else:
+        #         nums1[k]=nums2[r]
+        #         r+=1
+        #     k+=1
+        # while l<m:
+        #     nums1[k]=nums1[l]
+        #     l+=1
+        #     k+=1
+        # while r<n:
+        #     nums1[k]=nums2[r]
+        #     r+=1
+        #     k+=1
         for i in range(len(nums2)):
-            nums1[m+i]=nums2[i] 
-        print(nums1 , )
+            nums1[m+i]=nums2[i]
         for i in range(1 , len(nums1)):
             j=i
-            while j>0 and nums1[j-1] > nums1[j]:
-                nums1[j-1]  , nums1[j]=nums1[j] , nums1[j-1]
+            while nums1[j-1] >=nums1[j] and j>0:
+                nums1[j-1] , nums1[j]=nums1[j] , nums1[j-1]
                 j-=1
+        
+        
+
+        
