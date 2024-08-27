48eb860c69b726b3755f7bba78204e27b7fcb9e4
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-27 19:06:08 +0300

    Time: 8220 ms (6.65%), Space: 17.3 MB (18.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,42 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result=[]
-        def ispalindrom(res):
-            return res==res[::-1]
+        maxima=defaultdict(list)
+        l=r=0
+        s=list(s)
         for i in range(len(s)):
-            # for j in range(i+1 , len(s)+1):
-            j=i+1
-            while j<len(s)+1:
-                res=s[i:j]
-                if ispalindrom(res) :
-                    result.append(res)
-                j+=1
-        resu=sorted(result , key=lambda x:len(x)  , reverse=True)
-        if len(resu)==0:
-            return ""
-        else:
-            return resu[0]
-        return resu[0]
-
-        # result=[]
-        # def ispalindrome(res):
-        #     return res==res[::-1]
-        
-        # for i in range(len(s)):
-        #     r=i+1
-        #     strs=''
-        #     while r<len(s)+1:
-        #         res=s[i:r]
-        #         if ispalindrome(res):
-        #             result.append(res)
-        #         r+=1
-        # result=sorted(result , key= lambda x:len(x) , reverse=True)
-        # print(result)
-        # return result[0] if len(result)!=0 else ""
-        
+            strs=""
+            for j in range(i, len(s)):
+                strs+=s[j]
+                if strs==strs[::-1]:
+                    maxima[j-i+1]=strs
+        return maxima[max(maxima)]
+                
 
 
-        
-        
-
-        
