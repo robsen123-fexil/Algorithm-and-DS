a85b911a4cd57fe760f8fb229915cfd0205a77a9
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-29 14:13:52 +0300

    Time: 37 ms (51.52%), Space: 16.5 MB (22.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = int(''.join(map(str, digits)))
-        num += 1
-        nums = list(map(int, str(num)))
-        return nums
+        # num = int(''.join(map(str, digits)))
+        # num += 1
+        # nums = list(map(int, str(num)))
+        # return nums
+        strs=""
+        for i in digits:
+            strs+=(str(i))
+        integ=str(int(strs)+1)
+        res=' '.join(integ)
+        result=res.split()
+        sol=[]
+        for i in result:
+            sol.append(int(i))
+        return sol
+                    
