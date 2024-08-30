82bf8e1027324bfeb1969388e956fe69a1665686
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-08-30 23:54:08 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1=sorted(list(s1))
+        perm=list(permutations(s1))
+        result=[]
+        for i in perm:
+            result.append(''.join(i))
         s2=list(s2)
-
-        for i in range(len(s2)-len(s1)+1):
-            res=sorted(s2[i:i+len(s1)])
-            if res==s1:
+        res=s2[:len(s1)]
+        if ''.join(res) in result:
+            return True
+        l=0
+        
+        for i in range(len(s1) , len(s2)):
+            res.remove(s2[l])
+            res.append(s2[i])
+            
+            if ''.join(res)  in result:
                 return True
-        return False
+            l+=1
+        else:
+            return False
