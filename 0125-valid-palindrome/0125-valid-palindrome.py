f8615eb7bb32af92f2b1990c3b60bb9f1a3fdaaa
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-09-07 01:59:01 +0300

    Time: 51 ms (25.95%), Space: 17.2 MB (45.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        result=""
-        fina=""
+        strs=""
         for i in s:
-            if i.isalnum():
-                result+=i
-        res=result.lower()
-        for i in range(len(res)-1, -1, -1):
-            fina+=res[i]
-        if fina==res:
-            return True
-        else:
-            return False
+            if i.isalpha():
+
+                strs+=i.lower()
+            elif i.isdigit():
+                strs+=i
+
+        l=0
+        
+        print(strs)
+        cnt=0
+        
+       
+        r=len(strs)-1
+        print(strs)
+        while l<r:
+            cnt+=strs[l]!=strs[r]
+            l+=1
+            r-=1
+        return cnt==0
