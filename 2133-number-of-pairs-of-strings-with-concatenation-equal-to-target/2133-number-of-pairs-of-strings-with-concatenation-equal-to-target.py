88721861d72f73f09f034bd29cbe6d8bfbe361e9
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-09-15 22:06:37 +0300

    Time: 55 ms (53.54%), Space: 16.5 MB (82.32%) - LeetHub

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -3,7 +3,7 @@ class Solution:
         count=0
         for i in range(len(nums)):
             for j in range(len(nums)):
-                if i!=j:
-                    if nums[i]+nums[j]==target:
-                        count+=1
+                if i!=j and (nums[i]+nums[j]==target):
+                    count+=1
+            
         return count
