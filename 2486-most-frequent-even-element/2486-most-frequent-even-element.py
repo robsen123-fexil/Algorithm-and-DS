842b77b7ec65da09eb0bb2d3b122630c69db6c1f
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-09-18 01:34:38 +0300

    Time: 235 ms (24.83%), Space: 17.2 MB (5.77%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -1,36 +1,20 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        # hashmap={}
-        # res=[]
-        # for i in nums:
-        #     if i in hashmap:
-        #         hashmap[i]+=1
-        #     else:
-        #         hashmap[i]=1
-        # result={}
-        # res=sorted(hashmap.items() , key = lambda item:(item[1])  , reverse=True)
-        # resu={ k : v for k , v in res }
-        # for key , value in res:
-        #     if key%2==0:
-        #         return key
-        
-        
-        # return -1
-        # count=Counter(nums)
-        # res={k : v for k ,  v in count.items() if k%2==0}
-        
-        # if res:
-        #     return max(res , key = res.get)
-        # else:
-        #     return -1
-        count=Counter(nums)
-        result=[num for num in count if num%2==0]
-        if not result:
+        cnt=Counter(nums)
+        res={}
+        for key , val in cnt.items():
+            if key%2==0:
+                res[key]=val
+        print(res)
+        maxima=float('-inf')
+        if res:
+           maxima=max(res , key=res.get)
+        else:
             return -1
-        maxima=max(result , key =lambda x:(count[x] , -x))
-        return maxima
-
-        
-       
-        
-        
+        vall=res[maxima]
+        resu=[]
+        for key , val in res.items():
+            if val==vall:
+                resu.append(key)
+        print(maxima)
+        return min(resu) if resu else -1
