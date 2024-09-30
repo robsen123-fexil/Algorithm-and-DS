b6717b29b5eb843079a1a27ef8970ba34987dad6
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-09-30 23:47:24 +0300

    Time: 227 ms (65.39%), Space: 19.5 MB (26.52%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,11 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        sums=l=count=0
         hsh={0:1}
-        sums=count=0
-        for i  , val in enumerate(nums):
-            sums+=val
-            diff=sums-k
-            if diff in hsh:
-                count+=hsh[diff]
-            hsh[sums]=hsh.get(sums , 0)+1
+        for i in range(len(nums)):
+            sums+=nums[i]
+            if sums-k in hsh:
+                count+=hsh[sums-k]
+            if sums not in hsh:
+                hsh[sums]=1
+            else:
+                hsh[sums]+=1
         return count
