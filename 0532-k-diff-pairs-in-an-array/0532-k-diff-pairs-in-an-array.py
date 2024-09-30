49bc351ce24e3c4b971c1355ce80e79194531016
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-09-30 23:38:26 +0300

    Time: 8532 ms (5.01%), Space: 19.7 MB (5.25%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -1,47 +1,18 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        # count=0
-        # result=[]
-        # for i in range(len(nums)):
-        #     for j in range(i+1  , len(nums)):
-        #         val=abs(nums[i] -nums[j])
-        #         if val==k:
-        #             res=[nums[i] , nums[j]]
-        #             res.sort()
-        #             if  res not in result:
-        #                result.append(res)
-        # print(result)
-        # return len(result)
-        # l=0
-        # r=len(nums)-1
-        # count=0
-        # nums.sort()
-        # while l<len(nums)-1:
-        #     while l<r:
-        #         val=abs(nums[l]-nums[r])
-        #         if val==k:
-        #             count+=1
-        #             break
-        #         r-=1   
-        #     while l<(len(nums)-1):
-        #         if nums[l+1] !=nums[l]:
-        #             l+=1
-        #             break
-        #         l+=1
-        #     r=len(nums)-1
-        # return count
-        hsh=Counter(nums)
-        print(hsh)
         count=0
-        if k==0:
-            for key, value in hsh.items():
-                if value >1:
+        res=set()
+        for i in range(len(nums)):
+            for j in range(i+1 , len(nums)):
+                if abs(nums[i]-nums[j])==k:
+                    print(nums[i] , nums[j])
+                    res.add((nums[i] , nums[j]))
                     count+=1
-        else:
-            for key , value in hsh.items():
-                if key+k in nums:
-                    count+=1
-        return count
-
-
-        
+        resu=[]
+        print(res)
+        for i in res:
+            print(i)
+            r=sorted(list(i))
+            if r not in resu:
+                resu.append(r)
+        return len(resu)
