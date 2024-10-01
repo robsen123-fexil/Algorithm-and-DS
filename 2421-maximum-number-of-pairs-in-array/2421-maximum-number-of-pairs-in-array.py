ec9c6202cdbb7015b28a080c70dff95916dbde10
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-01 21:31:27 +0300

    Time: 46 ms (7.88%), Space: 16.6 MB (7.88%) - LeetHub

--- 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
+++ 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
@@ -1,17 +1,11 @@
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
         cnt=Counter(nums)
-        count=r=0
-        res=[]
-        print(cnt)
-        for key , val in cnt.items():
+        count=res=0
+        for val in cnt.values():
             if val%2==0:
-                print(key , val)
                 count+=(val//2)
             else:
+                res+=1
                 count+=(val//2)
-                r+=1
-        
-        res.append(count)
-        res.append(r)
-        return res
+        return [count , res]
