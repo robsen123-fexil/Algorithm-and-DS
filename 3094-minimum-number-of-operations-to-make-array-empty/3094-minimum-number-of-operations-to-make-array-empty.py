c8a77b27bc4990c13f79f633c0bdc2ea2860ea74
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-01 17:32:33 +0300

    Time: 577 ms (78.58%), Space: 30.8 MB (87.57%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.py
@@ -1,13 +1,12 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        count=0
         cnt=Counter(nums)
+        count=0
         for val in cnt.values():
+            if val<2:
+                return -1
             if val%3==0:
                 count+=(val//3)
-                
-            elif val==1:
-                return -1
             else:
                 count+=(val//3)+1
         return count
