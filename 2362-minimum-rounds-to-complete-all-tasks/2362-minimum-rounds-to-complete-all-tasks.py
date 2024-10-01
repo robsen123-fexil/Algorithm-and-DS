9f3821aef674b483f5d9eac5a6ba6cea9d217d81
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-01 17:29:42 +0300

    Time: 699 ms (85.52%), Space: 31.7 MB (49.9%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
@@ -1,30 +1,14 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
         cnt=Counter(tasks)
-        for val in cnt.values():
-            if val==1:
-                return -1
         count=0
-        upper=[]
-        for key , value in cnt.items():
-            if value == 3 or value == 2:
-                count+=1
+        for val in cnt.values():
+            if val<2:
+                return -1
+            if val%3==0:
+                count+=(val//3)
+            elif val==2:
+                count+=(1)
             else:
-                upper.append([])
-                for i in range(value):
-                    upper[-1].append(key)
-        print(count , upper)
-        
-        if upper:
-            for i in upper:
-                cnt=Counter(i)
-                print(cnt)
-                for key , value in cnt.items():
-                    if value%3==0:
-                        count+=(value//3)
-                    else:
-                        count+=(value//3)+1      
+                count+=(val//3)+1
         return count
-                        
-    
-            
