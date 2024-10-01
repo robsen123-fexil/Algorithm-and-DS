649a253df0c35be37c2f9d115cf74960edc972cd
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-01 08:55:10 +0300

    Time: 676 ms (38.11%), Space: 30.7 MB (67.56%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.py
@@ -1,26 +1,14 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        # result=[0]*(n+1)
-        # for i in bookings:
-        #     j , k , l =i
-        #     result[j]+=l
-        #     if k+1 < len(result):
-        #         result[k+1]-=l
-        # sums=0
-        # for i in range(1 , len(result)):
-        #     sums+=result[i]
-        #     result[i]=sums
-        # return result[1:]
-        result=[0]*(n+1)
+        result=[0]*n
         for i in range(len(bookings)):
-            x , y , z=bookings[i]
-            result[x]+=z
-            if (y+1)<len(result):
-                result[y+1]-=z
+            x , y , z =bookings[i]
+            result[x-1]+=z
+            if (y-1)+1 < len(result):
+                result[(y-1)+1]-=z
         sums=0
-        for i in range( len(result)):
+        res=[]
+        for i in range(len(result)):
             sums+=result[i]
-            result[i]=sums
-        return result[1:]
-            
-            
+            res.append(sums)
+        return res
