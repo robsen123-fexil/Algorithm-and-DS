088f0908834c45da0a8cfd1d5c3ae773f4e9649e
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-02 13:48:47 +0300

    Time: 46 ms (14.82%), Space: 16.6 MB (56.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,34 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # l=r=k=0
-        # for i in range(len(nums2)):
-        #     nums1[m+i]=nums2[i]
-        # print(nums1)
-        # while l<m and r<n:
-        #     if nums1[l]<=nums2[r]:
-        #         nums1[k]=nums1[l]
-        #         l+=1
-        #     else:
-        #         nums1[k]=nums2[r]
-        #         r+=1
-        #     k+=1
-        # while l<m:
-        #     nums1[k]=nums1[l]
-        #     l+=1
-        #     k+=1
-        # while r<n:
-        #     nums1[k]=nums2[r]
-        #     r+=1
-        #     k+=1
-        for i in range(len(nums2)):
-            nums1[m+i]=nums2[i]
-        for i in range(1 , len(nums1)):
+        l=m
+        r=0
+        while l<len(nums1) and r<n:
+            nums1[l]=nums2[r]
+            r+=1
+            l+=1
+        for i in range(len(nums1)):
             j=i
-            while nums1[j-1] >=nums1[j] and j>0:
-                nums1[j-1] , nums1[j]=nums1[j] , nums1[j-1]
+            while nums1[j]<nums1[j-1] and j>0:
+                nums1[j] , nums1[j-1]=nums1[j-1] , nums1[j]
                 j-=1
-        
-        
-
         
