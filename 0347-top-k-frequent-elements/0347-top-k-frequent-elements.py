bab49237c525a46185fbc752ef5e2b2af0c14c49
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-02 14:47:09 +0300

    Time: 90 ms (50.15%), Space: 21.2 MB (45.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,45 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # hashmap1={}
-        # for num in nums:
-        #     if num in hashmap1:
-        #         hashmap1[num]+=1
-        #     else:
-        #         hashmap1[num]=1
-        # val=sorted(hashmap1.items(), key=lambda x:x[1])
-        # if len(val)>=k:
-        #     ans=[key for key, _ in val[-k:]]
-        # else:
-        #     ans=[key for key, _ in val]
-        # return ans
-        # hashmap1={}
-        # for i in nums:
-        #     if i in hashmap1:
-        #         hashmap1[i]+=1
-        #     else:
-        #         hashmap1[i]=1
-        # val =sorted(hashmap1.items() , key =lambda x:x[1])
-        # ans=[]
-        # if len(val)>=k:
-        #     ans=[key for key , _ in val[-k:]]
-        # else:
-        #     ans=[key for key, _ in val]
-        # return ans
-        count=Counter(nums)
-        count=dict(sorted(count.items() , key =lambda x:x[1] ,  reverse=True) ,)
-        print(count)
-        result=[]
+        cnt=Counter(nums)
+        res=[]
         l=0
-        sol=[]
-        for key , value in count.items():
-            result.append(key)
-        while k>0:
-            sol.append(result[l])
-            l+=1
-            k-=1
-
-        return sol
-
-
-        result=[]
+        print(cnt)
+        cnt=dict(sorted(cnt.items() , key=lambda x:x[1]   , reverse=True))
+        print(cnt)
+        res=[k for k in cnt.keys()]
+        print(res)
+        return res[:k]
         
+
+
