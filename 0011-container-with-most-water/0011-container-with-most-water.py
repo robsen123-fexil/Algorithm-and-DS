05f9833af09d000f3b9267ea2a2eb38f8d917427
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-05 13:34:15 +0300

    Time: 533 ms (34.54%), Space: 29.9 MB (7.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,39 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # l=0
-        # r=len(height)-1
-        # maxima=float("-inf")
-        # while l<r:
-        #     leng=(r-l)
-        #     if height[l]>height[r]:
-        #         h=height[r]
-        #         r-=1
-        #     else:
-        #         h=height[l]
-        #         l+=1
-        #     maxima=max(maxima, leng*h)
-        # return maxima
-        
-        # l=0
-        # maxima=float('-inf')
-        # for i in range(len(height)):
-        #    r = len(height)-1
-        #    while i < r:
-        #       leng = r - i
-        #       value = min(height[i], height[r])
-        #       maxima = max(maxima, value * leng)
-        #       r -= 1
-        # return maxima
-        l , r , maxima = 0 , len(height)-1 , 0
+        l=0
+        r=len(height)-1
+        maxima=float('-inf')
         while l<r:
-            leng=r-l
-            value=min(height[l] , height[r])
-            maxima=max(maxima , value*leng)
-            if height[r]<height[l]:
-                r-=1
-            elif height[r]==height[l]:
-                r-=1
-            else:
+            minima=min(height[l] , height[r])
+            area=minima*(r-l)
+            maxima=max(maxima , area)
+            if height[l]<height[r]:
                 l+=1
+            else:
+                r-=1
         return maxima
-            
