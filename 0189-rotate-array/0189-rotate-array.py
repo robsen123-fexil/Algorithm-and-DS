95be3c30027438edca36f84a8f3705594495b629
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-15 20:53:07 +0300

    Time: 150 ms (87.98%), Space: 28.1 MB (37.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n=len(nums)
-        k=k%n
+        k%=len(nums)
         nums[:]=nums[-k:]+nums[:-k]
-        return nums
+        
+        
+        
