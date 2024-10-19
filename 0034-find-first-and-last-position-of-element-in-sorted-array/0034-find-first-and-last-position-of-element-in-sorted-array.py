ff3f43603113460e772de472af23285cd470ef56
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-10-19 23:25:06 +0300

    Time: 0 ms (100%), Space: 18.2 MB (12.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,66 +1,8 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l,r=0, len(nums)-1
-        ans=[]
-        we_get = -1
-        while l<=r:
-            mid=(l+r) // 2
-            if nums[mid] < target:
-                l=mid+1
-            elif nums[mid] > target:
-                r=mid-1
-            else:
-                we_get = mid
-                r = mid-1
-        ans.append(we_get)
-        we_get = -1
-        l,r=0, len(nums)-1
-        while l<=r:
-            mid=(l+r) // 2
-            if nums[mid] < target:
-                l=mid+1
-            elif nums[mid] > target:
-                r=mid-1
-            else:
-                we_get = mid
-                l = mid+1
-        ans.append(we_get)
-
-        return ans
-        # l=0
-        # ans=[]
-        # r=len(nums)-1
-        # we_get=-1
-        # while l <=r:
-        #     mid=(l+r) // 2
-        #     if nums[mid] < target:
-        #         l=mid+1
-        #     elif nums[mid] > target:
-        #         r=mid-1
-        #     else:
-        #         we_get=mid
-        #         r=mid-1
-        # ans.append(we_get)
-       
-        # we_get=-1
-        # l=0
-        # r=len(nums)-1
-        # while l<r:
-        #     mid=(l+r) // 2
-        #     if nums[mid] < target:
-        #         l=mid+1
-        #     elif nums[mid]> target:
-        #         r=mid-1
-        #     else:
-        #         we_get=mid
-        #         l=mid+1
-        # ans.append(we_get)
-        # return sorted(ans)
-             
-       
-
-
-       
-        
-
-    
+        hsh=defaultdict(list)
+        result=[]
+        for i in range(len(nums)):
+            if nums[i]==target:
+                result.append(i)
+        return [result[0] , result[-1]] if result else [-1 , -1]
