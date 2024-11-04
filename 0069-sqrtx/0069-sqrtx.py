243e6ddc329ae203160a0e81bc7e0706526d3398
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-05 00:20:57 +0300

    Time: 1918 ms (5.02%), Space: 16.7 MB (17.3%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,10 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**(0.5))
+        for i in range(x+1):
+            
+            if i**2 == x:
+                return i
+            elif i**2 > x:
+                return i-1
+        
+        return 0
