ecbc7b6929843b0dbe83ba838544ac5e874e1175
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-07 21:55:03 +0300

    Time: 11 ms (5.28%), Space: 27.5 MB (5.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,14 +1,7 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        hashmap1 ={}
-        for i in nums:
-            if i in hashmap1:
-                hashmap1[i]+=1
-            else:
-                hashmap1[i]=1
-        return min(hashmap1, key=hashmap1.get)
-        
-      
-
-
-        
+        cnt=Counter(nums)
+        mini=min(cnt.values())
+        for key , val in cnt.items():
+            if mini==val:
+                return key
