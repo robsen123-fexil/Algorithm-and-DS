3e138d306f2f74ef3936fc3698d458e95739b2ed
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-09 00:56:57 +0300

    Time: 27 ms (76.64%), Space: 17.1 MB (95.64%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.py
@@ -1,10 +1,12 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        sums=l=maxima=0
+        
+        sums=maxima=l=0
         for i in range(len(s)):
-            sums+=abs(ord(s[i])-ord(t[i]))
+            sums+=(abs(ord(s[i])-ord(t[i])))
             while sums>maxCost:
-                sums-=(abs(ord(s[l])-ord(t[l])))
+                sums-=abs(ord(s[l])-ord(t[l]))
                 l+=1
-            maxima=max(maxima, i-l+1)
+            
+            maxima=max(maxima , i-l+1)
         return maxima
