928a46ebeba9aecaf6dbb7ae2338233ca468e612
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-15 21:51:20 +0300

    Time: 5 ms (56.19%), Space: 16.8 MB (7.08%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,18 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         hsh={}
-        for i  , val in enumerate(s):
+        for i , val in enumerate(s):
             hsh[val]=i
-        maxima=hsh[s[0]]
+        print(hsh)
+        maxima=0
         strs=""
         res=[]
         for i in range(len(s)):
             maxima=max(maxima , hsh[s[i]])
             strs+=s[i]
-            if i==maxima:
-                print(strs)
+            if maxima==i:
                 res.append(len(strs))
                 strs=""
-            
         return res
-            
+
+
+        
