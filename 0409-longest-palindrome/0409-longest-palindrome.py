d5e40d1c56cdf03c4b94377e555eebe1539934ff
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-25 22:49:49 +0300

    Time: 0 ms (100%), Space: 16.7 MB (57.53%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        cnt=Counter()
-        count=0
-        for i in range(len(s)):
-            cnt[s[i]]+=1
-            if cnt[s[i]]%2==0:
-                count+=2
-        print(count)
-        for i in cnt.values():
-            if i%2!=0:
-                count+=1
-                break
-        return count
+        cnt=Counter(s)
+        sums=0
+        odd=False
+        for val in cnt.values():
+            if val%2==0:
+                sums+=val
+            else:
+                sums+=(val-1)
+                odd=True
+        if odd:
+            sums+=1
+        return sums
