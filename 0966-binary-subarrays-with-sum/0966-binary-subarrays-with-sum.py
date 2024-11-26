ace5f5092cdffac4c85200eafb7d92d9aaa796e5
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-11-26 08:51:09 +0300

    Time: 32 ms (48.75%), Space: 20.5 MB (33.15%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         hsh={0:1}
-        count=sums=0
+        sums=count=0
         for i in range(len(nums)):
             sums+=nums[i]
             if sums-goal in hsh:
                 count+=hsh[sums-goal]
+                
             if sums not in hsh:
                 hsh[sums]=1
             else:
