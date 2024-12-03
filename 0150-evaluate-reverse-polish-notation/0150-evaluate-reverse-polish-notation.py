6cd2cc5f6f58ba07d8bb4e9e10e6824aeeb44217
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-12-03 10:21:41 +0300

    Time: 3 ms (65.48%), Space: 19.2 MB (5.86%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,19 +1,23 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack=[]
-        for i in tokens:
-            if i =='+':
-                stack.append(stack.pop()+stack.pop())
-            elif i == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif i=='-':
-                a=stack.pop()
-                b=stack.pop()
-                stack.append(int(b-a))
-            elif i=='/':
-                a=stack.pop()
-                b=stack.pop()
-                stack.append(int(b/a))
+        oper=["+" , '/' , '*' , '-']
+        for i in range(len(tokens)):
+            if tokens[i] not in oper:
+                stack.append(int(tokens[i]))
             else:
-                stack.append(int(i))
+                a=stack.pop()
+                b=stack.pop()
+                if tokens[i]=='+':
+                    stack.append(b+a)
+                elif tokens[i]=='/':
+                    stack.append(int(b/a))
+                    
+                elif tokens[i]== '-':
+                    stack.append(b-a)
+                else:
+                   
+                    stack.append(b*a)
+                    
+        
         return stack[0]
