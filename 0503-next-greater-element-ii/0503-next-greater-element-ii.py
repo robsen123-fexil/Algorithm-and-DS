fc09ca6c31c262c3a8bf8d67c40f8a28b56bc937
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2024-12-04 13:55:37 +0300

    Time: 26 ms (50.31%), Space: 20 MB (5.92%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -2,17 +2,17 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         stack=[]
         result=[-1]*len(nums)
-        for ind , val in enumerate(nums):
-            while stack and stack[-1][1] < val:
-                index , value =stack.pop()
-                result[index]=val
-            stack.append([ind , val])
-        print(result)
-        print(stack)
-        for i  , val in enumerate(nums):
-            while stack and stack[-1][1]<val:
-                index , value = stack.pop()
-                result[index]=val
-                
-        print(result)
-        return result     
+        for i in range(len(nums)):
+            if stack :
+                while stack and stack[-1][0]<nums[i]:
+                    a , b = stack.pop()
+                    result[b]=nums[i]
+                stack.append([nums[i] , i])
+            else:
+                stack.append([nums[i] , i ])
+        print(result , stack)
+        for i , val in enumerate(nums):
+            while stack and stack[-1][0]<val:
+                a , b =stack.pop()
+                result[b]=val
+        return result
